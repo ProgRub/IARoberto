8324dbe2f05b6d9c299871c5a49114276ed5cc3d
ProgRub <ruben2000jgr@gmail.com>
2020-11-10 16:23:25 +0000

    Constantes e ligacoes

--- main.py
+++ main.py
@@ -5,15 +5,14 @@ from ev3dev2.sensor import INPUT_1
 from ev3dev2.sensor.lego import TouchSensor
 from ev3dev2.led import Leds
 
-ts = TouchSensor()
-leds = Leds()
+TAMANHO_TABULEIRO = 6
+DIREITA = 1
+ESQUERDA = -1
+CIMA = TAMANHO_TABULEIRO
+BAIXO = -TAMANHO_TABULEIRO
+ROBOT_NORTE = -10
+ROBOT_ESTE = -9
+ROBOT_OESTE = -8
+ROBOT_SUL=-7
 
-print("Press the touch sensor to change the LED color!")
-
-while True:
-    if ts.is_pressed:
-        leds.set_color("LEFT", "GREEN")
-        leds.set_color("RIGHT", "GREEN")
-    else:
-        leds.set_color("LEFT", "RED")
-        leds.set_color("RIGHT", "RED")
+tabuleiro=[]
